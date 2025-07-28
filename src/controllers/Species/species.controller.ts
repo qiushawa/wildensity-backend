4e381438187e99df72ff86683558b16fd574a3f5
邱聖傑(實驗室) <qiushawa@gmail.com>
2025-07-28 17:34:26 +0800

    建立物種模型和基礎路由

---
+++ src/controllers/Species/species.controller.ts
@@ -0,0 +1,21 @@
+import { prisma } from '@/common/database';
+import { RESPONSE_CODE } from '@/common/code';
+import { errorResponse, successResponse } from '@/common/response';
+
+import { Request, Response } from 'express';
+
+export class SpeciesController {
+    async getAllSpecies(req: Request, res: Response): Promise<void> {
+        const species = await prisma.species.findMany();
+        return successResponse(res, RESPONSE_CODE.SUCCESS, species);
+    }
+
+    async getSpecies(req: Request, res: Response): Promise<void> {
+        const speciesId = parseInt(req.params.speciesId, 10);
+        const species = await prisma.species.findUnique({
+            where: { species_id: speciesId },
+        });
+        if (species) return successResponse(res, RESPONSE_CODE.SUCCESS, species);
+        return errorResponse(res, RESPONSE_CODE.NOT_FOUND, '物種不存在');
+    }
+}
