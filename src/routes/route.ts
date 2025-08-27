a2583cbc3538bbe6a69eeeac7ac289a6addfe280
邱聖傑(實驗室) <qiushawa@gmail.com>
2025-08-27 09:57:25 +0800

    刪除未使用的路由

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,12 +1,8 @@
-import e, { Router } from 'express'
+import { Router } from 'express'
 import speciesRouter from './species.route'
 import areaRouter from './area.route'
-import { prisma } from '../common/database'
 
 const apiRouter = Router()
-apiRouter.get('/message', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
-})
 
 apiRouter.use('/species', speciesRouter);
 apiRouter.use('/areas', areaRouter);
